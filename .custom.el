3420bf51a8dc2072fdc06b66bf895c7268312722
mozeroman <mozeroman@gmail.com>
2014-12-05 11:31:48 +0800

    find a way to define ,. for eim in .custom.el

--- .custom.el
+++ .custom.el
@@ -4,6 +4,16 @@
 ;(define-prefix-command 'ctrl-z-map)
 ;(global-set-key (kbd "C-z") 'ctrl-z-map)
 
+;; 设定eim使得,.为选字翻页键
+(defun my-eim-py-activate-function ()
+  (add-hook 'eim-active-hook
+            (lambda ()
+              (let ((map (eim-mode-map)))
+                (define-key eim-mode-map "," 'eim-previous-page)
+                (define-key eim-mode-map "." 'eim-next-page)))))
+
+
+
 ;; all backup goto ~/.backups instead in the current directory
 (setq backup-directory-alist (quote (("." . "~/.backupsforEmacs"))))
 
