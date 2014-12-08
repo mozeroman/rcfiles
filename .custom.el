a00a65a38fe45f9b5fbc4719344f21ece8841977
mozeroman <mozeroman@gmail.com>
2014-12-08 19:39:28 +0800

    try new theme in emacs but fail

--- .custom.el
+++ .custom.el
@@ -1,8 +1,5 @@
-;;默认的Ctrl-z是什么来着，关闭emacs吧，好像还是强行关闭，不好，用C-x c就好了
-;(global-set-key (kbd "C-x c") 'suspend-frame)
-;;我就将Ctrl-z作为我的键的绑定
+;; 自定prefix
 ;(define-prefix-command 'ctrl-z-map)
-;(global-set-key (kbd "C-z") 'ctrl-z-map)
 
 ;;增加载入的配置文件夹路径,对应.emacs.d/lisp里面的配置文件
 (add-to-list 'load-path "~/.myemacs.d")
@@ -14,3 +11,6 @@
 
 ;; C-z 设置标记来进行区块选择
 (global-set-key (kbd "C-z") 'set-mark-command)
+
+
+
