a41f2ba829608337328635e73f6114a58bfcca90
mozeroman <mozeroman@gmail.com>
2014-12-08 22:38:57 +0800

    add % and filesize in modeline

--- .custom.el
+++ .custom.el
@@ -6,10 +6,8 @@
 (add-to-list 'load-path "~/.myemacs.d")
 (load "my-eim.el") ;;修改使用eim',''.'切换输入框翻页
 (load "my-org.el") ;;增加org-mobile和org-publish相关配置
-(load "molokai-theme-kit.el") ;;改变color-theme里面的之前的数字默认空格
+(load "molokai-theme-kit.el") ;;自定义color-theme-molokai
 
 ;; ALL backup goto ~/.backups instead in the current directory
 (setq backup-directory-alist (quote (("." . "~/.backupsforEmacs"))))
 
-;; 使用 color-theme-molokai 的拓展配置
-(setq molokai-theme-kit t)
--- .myemacs.d/molokai-theme-kit.el
+++ .myemacs.d/molokai-theme-kit.el
@@ -1,90 +1,54 @@
-;;; molokai-theme-kit.el
-
-;; Copyright (C) 2013 Huang Bin
-
-;; Author: Huang Bin <embrace.hbin@gmail.com>
-;; URL: https://github.com/hbin/molokai-theme
-;; Version: 0.8
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;;; Commentary:
-;;
-;; This is optional UI settings for molokai theme.
-;;
-;; Include: limun style, mode line...
-
-;;; Requirements:
-;;
-;; Emacs 24
-
-;;; Code:
-
-;;
-;; linum style
-;;
-(require 'linum)
-(setq linum-format "%3d ")
-
-(toggle-indicate-empty-lines nil)
-
-;;
 ;; mode-line
-;;
+;; editing from .emacs.d/lisp/init-modeline.el
 ;; http://emacs-fu.blogspot.com/2011/08/customizing-mode-line.html
+;; @see http://www.delorie.com/gnu/docs/elisp-manual-21/elisp_360.html
 (setq-default mode-line-format
-              (list
+  (list
+    ;; the buffer name; the file name as a tool tip
+    '(:eval (propertize "%b " 'face nil
+        'help-echo (buffer-file-name)))
+    ;; line and column
+    "(" ;; '%02' to set to 2 chars at least; prevents flickering
+    "%02l" "," "%01c"
+      ;; (propertize "%02l" 'face 'font-lock-type-face) ","
+      ;; (propertize "%02c" 'face 'font-lock-type-face)
+    ") "
 
-               ;; the buffer name; the file name as a tool tip
-               " " '(:eval (propertize "%b " 'face 'font-lock-keyword-face 'help-echo (buffer-file-name)))
+    ;; relative position, size of file
+    "[" (propertize "%p" 'face 'nil) "/" (propertize "%I" 'face 'nil) "] "
 
-               ;; line and column
-               "(" (propertize "%l" 'face 'font-lock-type-face) "," (propertize "%c" 'face 'font-lock-type-face) ") "
+    ;; the current major mode for the buffer.
+    "["
 
-               ;; relative position, size of file
-               "[" (propertize "%p" 'face 'font-lock-constant-face) "/" (propertize "%I" 'face 'font-lock-constant-face) "] "
+    '(:eval (propertize "%m" 'face nil
+              'help-echo buffer-file-coding-system))
+    " "
 
-               ;; the current major mode for the buffer.
-               "[" '(:eval (propertize "%m" 'face 'font-lock-string-face 'help-echo buffer-file-coding-system)) "] "
 
-               "[" ;; insert vs overwrite mode, input-method in a tooltip
-               '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
-                                   'face 'font-lock-preprocessor-face
-                                   'help-echo (concat "Buffer is in "
-                                                      (if overwrite-mode "overwrite" "insert") " mode")))
-               ;; was this buffer modified since the last save?
-               '(:eval (when (buffer-modified-p)
-                         (concat ","  (propertize "Mod"
-                                                  'face 'font-lock-warning-face
-                                                  'help-echo "Buffer has been modified"))))
-               ;; is this buffer read-only?
-               '(:eval (when buffer-read-only
-                         (concat ","  (propertize "RO"
-                                                  'face 'font-lock-type-face
-                                                  'help-echo "Buffer is read-only"))))
-               "] "
-               ;; add the time, with the date and the emacs uptime in the tooltip
-               ;;'(:eval (propertize (format-time-string "%H:%M")
-               '(:eval (propertize (format-time-string  "%A, %B %d, %Y -- %H:%M")
-                                   'help-echo
-                                   (concat (format-time-string "%c; ")
-                                           (emacs-uptime "Uptime:%hh"))))
+    ;; insert vs overwrite mode, input-method in a tooltip
+    '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
+              'face nil
+              'help-echo (concat "Buffer is in "
+                           (if overwrite-mode "overwrite" "insert") " mode")))
 
-               " --"
-               ;; i don't want to see minor-modes; but if you want, uncomment this:
-               ;; minor-mode-alist  ;; list of minor modes
-               "%-" ;; fill with '-'
-               ))
+    ;; was this buffer modified since the last save?
+    '(:eval (when (buffer-modified-p)
+              (concat ","  (propertize "Mod"
+                             'face nil
+                             'help-echo "Buffer has been modified"))))
 
-(provide 'molokai-theme-kit)
+    ;; is this buffer read-only?
+    '(:eval (when buffer-read-only
+              (concat ","  (propertize "RO"
+                             'face nil
+                             'help-echo "Buffer is read-only"))))
+    "] "
+
+    ;;global-mode-string, org-timer-set-timer in org-mode need this
+    (propertize "%M" 'face nil)
+
+    " --"
+    ;; i don't want to see minor-modes; but if you want, uncomment this:
+    ;; minor-mode-alist  ;; list of minor modes
+    "%-" ;; fill with '-'
+    ))
