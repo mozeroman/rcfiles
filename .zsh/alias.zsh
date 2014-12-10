768f4111ac3c4cf50d134b7ce8fdf1d0b8f85a06
mozeroman <mozeroman@gmail.com>
2014-12-10 10:31:29 +0800

    change .zsh/.* into .zsh/*

---
+++ .zsh/alias.zsh
@@ -0,0 +1,13 @@
+alias emacs='emacs -nw'
+alias minecraft='~/.minecraft/HMCL-2.0.9-proguard-Linux.sh'
+alias moc='mocp'
+alias sai="sudo apt-get install"
+alias sar="sudo apt-get remove"
+alias sap="sudo apt-get purge"
+alias autoremove="sudo apt-get autoremove"
+alias autoclean="sudo apt-get autoclean"
+alias matlab="/media/ubuntu/usr/local/MATLAB/R2012a/bin/matlab"
+alias logout="xfce4-session-logout"
+alias poweroff="sudo poweroff"
+alias reboot="sudo reboot"
+alias devilspie="devilspie -a &  sleep 2 && pkill -f 'devilspie -a'"  
