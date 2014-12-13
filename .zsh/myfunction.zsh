c772f8ec40105e23892a05e61bc3f7b2476e1cd0
mozeroman <mozeroman@gmail.com>
2014-12-13 18:07:19 +0800

    find out lss only work single file/dir

--- .zsh/myfunction.zsh
+++ .zsh/myfunction.zsh
@@ -47,41 +47,51 @@ scopy() {
        fi
 }
 
-# ## a function for selection rm | percol
-# rms() {
-#     rm $(ls | percol) -rf
-# }
-# ###problem accour with function-name scp comflict with ^R
-# ## a function for selection cp | percol
-# cps() {
-#     cp -i $(ls | percol) $1
-# }
-# ## a function for selection mv | percol
-# mvs() {
-#     mv -i $(ls | percol) $1
-# }
-# ## a function for selection cd | percol
-# cds() {
-#     cd $(ls | percol)
-# }
-# ## a function for selection extract | percol
-# extracts() {
-#     extract $(ls | percol)
-# }
+## a function for selection rm | percol
+rms() {
+    rm $(ls | percol) -rf
+}
+###problem accour with function-name scp comflict with ^R
+## a function for selection cp | percol
+cps() {
+    cp -i $(ls | percol) $1
+}
+## a function for selection mv | percol
+mvs() {
+    mv -i $(ls | percol) $1
+}
+## a function for selection cd | percol
+cds() {
+    cd $(ls | percol)
+}
+## a function for selection extract | percol
+extracts() {
+    extract $(ls | percol)
+}
 
+## wouldn't seperate $dir and it just a WHOLE-STRING, so only one file can be seleted
 ## a function combine ls&percol and other program, sudo su might be useful
 lss() {
-   if [ -f $1 ] ; then
+   dir=$(ls | percol);
+   #dir=$(ls | percol | tr '\n' ' ');
+   #echo ${dir/\n/ };
+   #echo $dir
+   #echo $dir | tr '\n' ' ';
+   #rdir=$($dir|sed 'N;s/\n//g');
+   #rdir=sed 'N;s/\n/ /g' $dir;
+   #rdir=$($dir | tr '\n' ‘ ’ );
+   #dir=sed 'N;s/\n/:/' $dir;
+   if [ $1 ] ; then
        case $1 in
-           emacs)  emacs -nw  $(ls | percol) $2 ;;
-           rm) rm $(ls | percol) -rf ;;
-           cp) cp -i $(ls | percol) $2;; ## ?? -i worldn't ask, just force
-           mv) mv -i $(ls | percol) $2;; ## ?? -i worldn't ask, just force
-           *)  $1 $(ls | percol) $2 ;;
+           emacs)  emacs -nw  $dir $2;;
+           rm) rm -rf $dir ;;
+           cp) cp -i $dir $2;; ## -i worldn't ask, just force
+           mv) mv -i $dir $2;; ## -i worldn't ask, just force
+           *)  $1 $dir $2 ;; ##will act like cd
        esac
    else
        ##will act like cd
-       $1 $(ls | percol) $2
+       $1 $dir $2
    fi
 }
 
