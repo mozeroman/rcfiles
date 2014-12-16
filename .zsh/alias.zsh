47103444c0f50fdb4f7541ed9e36e7f562c38673
mozeroman <mozeroman@gmail.com>
2014-12-16 21:41:15 +0800

    add update for apt-get

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -2,6 +2,7 @@ alias emacs='emacs -nw'
 alias e='emacs -nw'
 alias minecraft='~/.minecraft/HMCL-2.0.9-proguard-Linux.sh'
 alias moc='mocp'
+alias sau="sudo apt-get update"
 alias sai="sudo apt-get install"
 alias sar="sudo apt-get remove"
 alias sap="sudo apt-get purge"
@@ -10,6 +11,6 @@ alias matlab="/media/ubuntu/usr/local/MATLAB/R2012a/bin/matlab &"
 alias logout="xfce4-session-logout"
 alias poweroff="sudo poweroff"
 alias reboot="sudo reboot"
-alias devilspie="devilspie -a & sleep 1 && pkill -f 'devilspie -a'"  
+alias devilspie="devilspie -a & sleep 1 && pkill -f 'devilspie -a'"
 alias lls="lss"
 
