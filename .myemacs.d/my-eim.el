9bb26e7fdc62ee0400d2072bbadccd1a1cbbf4fb
mozeroman <mozeroman@gmail.com>
2014-12-18 22:11:54 +0800

    remove my-eim.py because C-n C-p for eim

--- .myemacs.d/my-eim.el
+++
@@ -1,9 +0,0 @@
-
-;; ;; 设定eim使得,.为选字翻页键
-;; (defun my-eim-py-activate-function ()
-;;   (add-hook 'eim-active-hook
-;;             (lambda ()
-;;               (let ((map (eim-mode-map)))
-;;                 (define-key eim-mode-map "," 'eim-previous-page)
-;;                 (define-key eim-mode-map "." 'eim-next-page)))))
-
