8ff078426de63a1b678c0ca3f2ce971693d3b1b0
mozeroman <mozeroman@gmail.com>
2014-12-19 23:02:15 +0800

    change links way in .custom.el & fix bug yanpy.txt

--- .custom.el
+++ .custom.el
@@ -2,11 +2,13 @@
 ;; (define-prefix-command 'ctrl-z-map)
 ;; (global-set-key (kbd "C-z") 'ctrl-z-map)
 
-;;增加载入的配置文件夹路径,对应.emacs.d/lisp里面的配置文件
-(add-to-list 'load-path "~/.myemacs.d")
-(load "my-eim.el") ;;修改使用eim',''.'切换输入框翻页
-(load "my-org.el") ;;增加org-mobile和org-publish相关配置
-(load "emacs-daemon.el") ;;使用emacs作为daemon打开,提高打开速度
+;;增加载入的配置文件路径,对应.emacs.d/lisp里面的配置文件
+(if (file-exists-p "~/.myemacs.d/my-org.el") (load-file "~/.myemacs.d/my-org.el"))
+(if (file-exists-p "~/.myemacs.d/emacs-daemon.el") (load-file "~/.myemacs.d/emacs-daemon.el"))
+
+;; (add-to-list 'load-path "~/.myemacs.d")
+;; (load "my-org.el") ;;增加org-mobile和org-publish相关配置
+;; (load "emacs-daemon.el") ;;使用emacs作为daemon打开,提高打开速度
 
 ;;ALL backup goto ~/.backups instead in the current directory
 (setq backup-directory-alist (quote (("." . "~/.backupsforEmacs"))))
