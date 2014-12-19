3ee69d3bfd8423f0220df2bf21d404aa7221d935
mozeroman <mozeroman@gmail.com>
2014-12-20 01:45:29 +0800

    new ways to link file in .custom.el

--- .custom.el
+++ .custom.el
@@ -1,14 +1,6 @@
-;; ;;自定prefix
-;; (define-prefix-command 'ctrl-z-map)
-;; (global-set-key (kbd "C-z") 'ctrl-z-map)
-
 ;;增加载入的配置文件路径,对应.emacs.d/lisp里面的配置文件
-(if (file-exists-p "~/.myemacs.d/my-org.el") (load-file "~/.myemacs.d/my-org.el"))
-(if (file-exists-p "~/.myemacs.d/emacs-daemon.el") (load-file "~/.myemacs.d/emacs-daemon.el"))
-
-;; (add-to-list 'load-path "~/.myemacs.d")
-;; (load "my-org.el") ;;增加org-mobile和org-publish相关配置
-;; (load "emacs-daemon.el") ;;使用emacs作为daemon打开,提高打开速度
+(if (file-exists-p "~/.myemacs.d/my-org.el") (load-file "~/.myemacs.d/my-org.el")) ;;增加org-mobile和org-publish相关配置
+(if (file-exists-p "~/.myemacs.d/emacs-daemon.el") (load-file "~/.myemacs.d/emacs-daemon.el")) ;;设定emacsclient -c的默认配置
 
 ;;ALL backup goto ~/.backups instead in the current directory
 (setq backup-directory-alist (quote (("." . "~/.backupsforEmacs"))))
