d608dd4c6eefb802aff8d2e0c67e7b6adba2897c
mozeroman <mozeroman@gmail.com>
2015-03-11 16:49:12 +0800

    using box/webdav as mobileorg sync, vimperatorrc?

--- .myemacs.d/my-org.el
+++ .myemacs.d/my-org.el
@@ -1,6 +1,6 @@
 ;;设置MobileOrg 和 org-publish 和 agenda的文件仅仅设置为GTD.org
 
-(defvar dir-where-you-store-org-files "~/Dropbox/org/org/")
+(defvar dir-where-you-store-org-files "~/Box/org/org/")
 (setq
  org-agenda-files
  (mapcar (lambda (x) (concat dir-where-you-store-org-files x))
@@ -9,9 +9,9 @@
 ;;网络使用dropbox同步org的publish
 (setq org-publish-project-alist
       '(("note-org"
-         :base-directory "~/Dropbox/org/org"
-         :publishing-directory "~/Dropbox/org/publish"
          :base-extension "org"
+         :base-directory "~/Box/org/org"
+         :publishing-directory "~/Box/org/publish"
          :recursive t
          :publishing-function org-publish-org-to-html
          :auto-index nil
@@ -23,7 +23,7 @@
     href=\"./style/emacs.css\"
     type=\"text/css\"/>")
         ("note-static"
-         :base-directory "~/Dropbox/org/org"
+         :base-directory "~/Box/org/org"
          :publishing-directory
          :recursive t
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|swf\\|zip\\|gz\\|txt\\|el"
@@ -36,16 +36,18 @@
 
 ;;这个是设置org-mobile同步的设置,注意中文使用utf-8不然别用
 ;需要导出的原始.org文件
-(setq org-mobile-files (list "~/Dropbox/org/org/GTD.org"))
+(setq org-mobile-files (list "~/Box/org/org/GTD.org"))
 ;平时.org文件在的文件夹
-(setq org-directory "~/Dropbox/org/org/")
+;(setq org-directory "~/Dropbox/org/org/")
+(setq org-directory "~/Box/org/org/")
 ;从mobile端传回的回馈,仍需手动修改原始.org文件
-(setq org-mobile-inbox-for-pull "~/Dropbox/org/org/inbox.org")
+(setq org-mobile-inbox-for-pull "~/Box/org/org/inbox.org")
 ;设置dropbox同步的.org文件位置
 ;(setq org-mobile-directory "~/Dropbox/应用/MobileOrg/")  ;;iphone/itouch使用者路径,由于界面没人性放弃了...
-(setq org-mobile-directory "~/Dropbox/org/MobileOrg/")
+(setq org-mobile-directory "~/Box/org/MobileOrg/")
 ;使用md5工具计算,不然mobile端不同步
-(defcustom org-mobile-checksum-binary (or (executable-find "~/Dropbox/md5sums-1.2/md5sums.exe") (or (executable-find "md5sum")))
+
+(defcustom org-mobile-checksum-binary (or (executable-find "~/Box/md5sums-1.2/md5sums.exe") (or (executable-find "md5sum")))
  "Executable used for computing checksums of agenda files."
  :group 'org-mobile
  :type 'string)
